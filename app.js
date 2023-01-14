1b310489e98fbdead8e3d30895941aa011f2af6b
DEWAN <dewanmahinchy@gmail.com>
2023-01-14 23:02:12 +0600

    save button handler added

--- app.js
+++ app.js
@@ -22,3 +22,21 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     const balance = document.getElementById('balance');
     balance.innerText = income - expenses;
 })
+// Save button handler
+document.getElementById('save-btn').addEventListener('click', function(){
+    // Income
+    const income = getInputValue('income');
+    // Save
+    const save = getInputValue('save');
+    // Percentage Calculation
+    const savingAmount = (income * save) / 100;
+    // Set Saving Amount
+    const previousSavingAmount = document.getElementById('saving-amount');
+    previousSavingAmount.innerText = savingAmount;
+    // get balance amount
+    const balanceElement = document.getElementById('balance');
+    const balance = parseFloat(balanceElement.innerText);
+    // set remaining balance
+    const remainingBalance = document.getElementById('remaining-balance');
+    remainingBalance.innerText = balance - savingAmount;
+})
